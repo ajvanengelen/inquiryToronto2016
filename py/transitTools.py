b5ced229dc5e30ff10ffd3469a3add77f14c4ceb
Alexander van Engelen <engelen@cita.utoronto.ca>
2016-07-08 11:26:44 -0400

    tweakin

--- py/transitTools.py
+++ py/transitTools.py
@@ -10,25 +10,30 @@ import pdb
 
 def timeSeries(N = 300, samplingRatePerDay = 1., amplitude = 1., kneeFreq = 1, powerLawIndex = 1, doPlots = False):
 
-
+    #time duration of each sample.
     lengthOfObservation = 1. / samplingRatePerDay
 
+    #duration of the whold dataset
     duration = N * lengthOfObservation
 
+    #random, uncorrelated (white) noiwe
     uncorrelated = random.randn(N)  #white/uncorrelated Gaussian noise 
-   
+
+    #fourier transform it.
     fftUncorrelated = fft.fft(uncorrelated)
 
-    dOmega = 2 * pi / duration
-
+    #get frequencies corresponding to the Fourier transform.
     fftFreqs = fft.fftfreq(N, d = lengthOfObservation)
 
+    #power spectrum white white and 1/f noise components.
     powerSpec = amplitude *(1 +  abs( (fftFreqs / kneeFreq)**(-1 * powerLawIndex)))
     powerSpec[0] = 0.
 
+    #multiply by square root of the power spectrum.
     fftCorrelated = fftUncorrelated * sqrt(abs(powerSpec))
 
 
+    #make plots if requested..
     if doPlots:
         plt.figure('power')
         plt.clf()
@@ -48,7 +53,7 @@ def timeSeries(N = 300, samplingRatePerDay = 1., amplitude = 1., kneeFreq = 1, p
         # pdb.set_trace()
 
     
-
+        #fourier transform back to real space.
     correlated = real(fft.ifft(nan_to_num(fftCorrelated)))
 
     # return (correlated, uncorrelated, [fftFreqs, powerSpec])
